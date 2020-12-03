b87f335be2424c984593e7731b35fe96dbeaba35
Korben Kirscht <kirscht@gmail.com>
2020-12-02 18:30:54 -0800

    Setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -o /etc/ansible/playbook_w12_d3_a.yml https://github.com/galaxy3-net/week-12-day-3/blob/week-12-day-3/ansible/playbook_w12_d3_a.yml
+curl -o /etc/ansible/playbook_w12_d3_a.yml https://raw.githubusercontent.com/galaxy3-net/week-12-day-3/week-12-day-3/ansible/playbook_w12_d3_a.yml
 
 ansible-playbook /etc/ansible/playbook_w12_d3_a.yml
 
